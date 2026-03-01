79e6f42cca512ea99e1cd661c9ec3e758c1e8cf2
Shirochi <noanimenolife03@gmail.com>
2026-03-01 16:25:30 +0400

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,3 +1,10 @@
 @echo off
+setlocal
 cd /d "%~dp0"
 python epub_combiner.py
+set EXITCODE=%ERRORLEVEL%
+echo.
+echo Program exited with code %EXITCODE%
+echo (Close this window or press any key to exit...)
+pause >nul
+exit /b %EXITCODE%
